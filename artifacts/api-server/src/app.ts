7cf706c87e3b05ef1d73c0c01b13030daaec1af7
shorellkano <shorell.kano@gmail.com>
2026-05-04 11:08:36 +0100

    Fix formatting and add pinoHttp middleware

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -11,9 +11,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app: Express = express();
-
-app.use( 
 // @ts-ignore
+app.use( 
+
 pinoHttp({
     logger,
     serializers: {
